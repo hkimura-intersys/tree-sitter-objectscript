2ae1abf7ac4b4d149dbb5626f5efc197c209a677
Hannah <hannah.kimura@intersystems.com>
2026-03-12 17:12:20 -0400

    fix: split up rust crates into playground and udl crates.

--- bindings/rust-playground/build.rs
+++ bindings/rust-playground/build.rs
@@ -2,13 +2,16 @@ use std::path::Path;
 
 fn main() {
     let root_dir = Path::new(".");
-    let obj_udl_dir = root_dir.join("udl").join("src");
+    let obj_script_dir = root_dir.join("objectscript").join("src");
     let common_dir = root_dir.join("common");
     let mut config = cc::Build::new();
 
-    config.include(&obj_udl_dir);
+    config.include(&obj_script_dir);
 
-    for path in &[obj_udl_dir.join("parser.c"), obj_udl_dir.join("scanner.c")] {
+    for path in &[
+        obj_script_dir.join("parser.c"),
+        obj_script_dir.join("scanner.c"),
+    ] {
         config.file(path);
         println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
     }
@@ -23,5 +26,5 @@ fn main() {
         config.define("TREE_SITTER_DISABLE_ATOMIC", None);
     }
 
-    config.compile("tree-sitter-objectscript");
+    config.compile("tree-sitter-objectscript-playground");
 }
